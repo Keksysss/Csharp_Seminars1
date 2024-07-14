d8ad67db5b0b4b880d34f96982bb560f8ce9f051
«Rita» <Keksys@icloud.com>
2024-07-14 21:42:18 +0300

    add task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, 
+// которая на вход принимает два числа и выводит,
+// какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine($"a = {firstNumber} большее, b = {secondNumber} меньшее");
+}
+else
+{
+    System.Console.WriteLine($"b = {secondNumber} большее, a = {firstNumber} меньшее");
+}
